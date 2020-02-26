b465553092490fb851deba7d841aaed097eae886
Erik Tonon <eriktonon@MacBook-Pro-de-Erik.local>
2020-02-25 21:52:07 -0300

    create shell from install vim config

---
+++ README.md
@@ -0,0 +1,17 @@
+# Install vim config
+
+`Follow the steps below to perform the installation`
+
+``` bash 
+ git clone https://github.com/eriktonon/vim-config.git
+```
+``` bash
+cd vim-config
+```
+
+``` bash
+ chmod +x install.sh
+```
+``` bash 
+ sh install.sh
+```
---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo '==================================='
+echo 'Install vim config'
+echo '==================================='
+sleep 3
+cp ./vimrc ~/.vimrc
+echo '==================================='
+echo 'installation completed successfully'
+echo '==================================='
+sleep 2
